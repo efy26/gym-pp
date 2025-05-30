a337b24233b4114182a9ff1eaa5475e834c080a7
efy26 <eliezerefoyaefy2018@gmail.com>
2025-05-30 01:36:43 -0400

    mise a jour sur ka section une

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,8 @@
 export default function Footer() {
     return (
-<<<<<<< HEAD
-        <footer className="flex justify-center h-15 items-center bg-red-900 text-white">
-            &copy; 2025 Gym
-=======
+
         <footer className="flex justify-center h-35 items-center text-white">
-            &copy; 2025 Gym-app, créer par le groupe gym-app
->>>>>>> design page home termine
+            &copy; 2025 Gym-app, créer par le groupe gym-app.
         </footer>
     );
 }
