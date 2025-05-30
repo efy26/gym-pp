29d204a8ea3a7ff63dabd4185b0ac90fa11796b4
efy26 <eliezerefoyaefy2018@gmail.com>
2025-05-30 08:47:01 -0400

    mise a jour design site

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import twitter from "@/public/img/twitter.webp";
+import instagram from "@/public/img/instagram.webp";
+import facebook from "@/public/img/facebook.jpg";
 export default function Footer() {
     return (
 
-        <footer className="flex justify-center h-35 items-center text-white">
-            &copy; 2025 Gym-app, créer par le groupe gym-app.
+        <footer className="flex justify-center flex-col h-35 items-center text-white mt-10">
+            <div id="img-block" className="flex justify-between w-30">
+                <Image src={facebook} alt="facebook" id="logo" className="size-7 cursor-pointer rounded-lg"/>
+                <Image src={instagram} alt="instagram" id="logo" className=" size-7 cursor-pointer rounded-lg"/>
+                <Image src={twitter} alt="x" id="logo" className=" size-7 cursor-pointer rounded-lg"/>
+                
+            </div>
+            <p className="mt-10">
+                &copy; 2025 Gym-app, créer par le groupe gym-app.
+            </p>
         </footer>
     );
 }
--- components/home/SectionUn.jsx
+++ components/home/SectionUn.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import logo from "@/public/img/logos.webp";
 export default function SectionUn({changePage}) {
     return (
-        <section className="bg-[url(@/public/img/strong.webp)] bg-cover bg-center bg-no-repeat min-w-100 min-h-170 flex  flex-col items-center">
-            <Image onClick={() => {changePage('home')}} src={logo} alt="Logo" id="logo" className="ml-2 size-40 mt-15 cursor-pointer rounded-lg ring-3"/>
-            <p id="citation-home" className="max-w-150 text-1xl font-mono mt-30">
+        <section id="section-un" className="bg-[url(@/public/img/image.webp)] bg-cover bg-center bg-no-repeat min-w-100 h-200 flex  flex-col items-center">
+            <Image onClick={() => {changePage('home')}} src={logo} alt="Logo" id="logo" className="size-40 mt-80 cursor-pointer"/>
+            <p id="citation-home" className="max-w-170 font-mono mt-10 text-shadow-lg/500">
                 "Chez nous, le fitness ne se limite
                 pas aux machines et aux haltères : 
                 c’est une famille soudée, où chaque membre se 
