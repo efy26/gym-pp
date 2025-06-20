2107708adee6b4943af90c923da962bba2fca07e
efy26 <eliezerefoyaefy2018@gmail.com>
2025-06-19 21:08:46 -0400

    installation emailjs, useForma et joute des fonctions

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,41 @@
+"use client";
 import React from 'react';
 import SectionUniverselle from "@/components/home/SectionUniverselle";
+import { useState } from 'react';
+import { useForm } from "react-hook-form";
+import emailjs from '@emailjs/browser'
+
+
 
 export default function Contact() {
+    const [result, setResult] = useState('');
+    const [success, setSuccess] = useState(true);
+     // Ininitialisation du formulaire avec react-hook-form
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        getValues,
+        reset,
+    } = useForm({
+        defaultValues: {
+            nom: "",
+            email: "",
+            sujet: "",
+            message: "",
+        },
+        mode: "onBlur", // Validation on blur
+    });
+
+    const sendEmail = () => {
+        const templateParams = {
+            name: getValues().nom,
+            email: getValues().email,
+            subject: getValues().sujet,
+            message: getValues().message,
+        };
+// ICI=============================================================
+       
     return (
         <div  className='flex flex-col'>
             <SectionUniverselle/>
@@ -15,14 +49,66 @@ export default function Contact() {
                             <p>Tel : 819-765-8600</p>
                             <p>E-mail : gym-app@gmail.com</p>
                         </span>
-                            <form>
+                            <form 
+                                onSubmit={handleSubmit(sendEmail)}
+                                noValidate
+                            >
                                 <div id='block-form'>
                                     <div id='input-text'>
-                                        <input type="text" placeholder='Nom'/>
-                                        <input type="email" placeholder='Mail'/>
-                                        <input type="tel" placeholder='Telephone'/>
+                                        <input
+                                            {...register("nom", {
+                                                required: "Champ obligatoire",
+                                            })}
+                                        type="text" placeholder='Nom'/>
+                                        {errors.nom && (
+                                        <div className="text-red-500 text-sm">
+                                            {errors.nom.message}
+                                        </div>
+                                    )}
+                                        <input 
+                                        {...register("email", {
+                                            required: "Champ obligatoire",
+                                            pattern: {
+                                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                                message: "Adresse email invalide",
+                                            },
+                                        })}
+                                        type="email" placeholder='Mail'/>
+                                        {errors.email && (
+                                            <div className="text-red-500 text-sm">
+                                                {errors.email.message}
+                                            </div>
+                                        )}
+                                        <input 
+                                        {...register("sujet", {
+                                            required: "Champ obligatoire",
+                                        })}
+                                        type="text" placeholder='sujet'/>
+                                        {errors.sujet && (
+                                            <div className="text-red-500 text-sm">
+                                                {errors.sujet.message}
+                                            </div>
+                                        )}
                                     </div>
-                                    <textarea name="" id="textarea" placeholder='Redigez votre message ici...'></textarea>
+                                    {/* <textarea 
+                                    {...register("message", {
+                                        required: "Champ obligatoire",
+                                    })}
+                                    name="" id="textarea" placeholder='Redigez votre message ici...'></textarea> */}
+
+                                    <textarea
+                        {...register("message", {
+                            required: "Champ obligatoire",
+                        })}
+                        id="textarea"
+                        rows="4"
+                        
+                    ></textarea>
+                                    {errors.message && (
+                                        <div className="text-red-500 text-sm">
+                                            {errors.message.message}
+                                        </div>
+                                    )}
                                 </div>
                                 <input id="btn-contact" type="submit" value="Envoyer" />
                             </form>
